d662c90abcc0d51440d7710811cef5bebb46a435
KillerjonserHD <jonathanmertens2006@gmail.com>
2021-08-17 10:14:48 +0200

    Added list of changes

--- cogs/settings.py
+++ cogs/settings.py
@@ -17,9 +17,20 @@ class Settings(commands.Cog):
             json.dump(data, file)
 
     @commands.command(brief='sets a specific setting')
-    async def settings(self, ctx, action, setting, *, param=None):
+    async def settings(self, ctx, action, setting=None, *, param=None):
         with open('data/data.json') as file:
             data = json.load(file)
+
+        listOfSettings = ['createvoicechannel', 'temptalkname', 'temptalkcategory', None]
+        if(setting not in listOfSettings):
+            embed=discord.Embed(
+                title='Error',
+                description=f'{setting} is an unknown setting. Use `-setting list` to get a full list of all settings',
+                color=discord.Color.from_rgb(255, 0, 0)
+            )
+            await ctx.send(embed=embed)
+            return
+
         if(action == 'set'):
             data[str(ctx.guild.id)][setting] = param
             embed=discord.Embed(
@@ -43,6 +54,17 @@ class Settings(commands.Cog):
                     description='You have to set the setting before accessing them',
                     color=discord.Color.from_rgb(255, 0, 0)
                 )
+        
+        elif(action == 'list'):
+            embed=discord.Embed(
+                title='Settings',
+                description='This is a list of all settings of the Bot',
+                color=discord.Color.from_rgb(0, 0, 255)
+            )
+            embed.add_field(name="createvoicechannel", value="The Channel you join to create a new channel", inline=False)
+            embed.add_field(name="temptalkname", value="The standart name of a channel", inline=False)
+            embed.add_field(name="temptalkcategory", value="The category, where the channels get created", inline=False)
+
         else:
             embed=discord.Embed(
                 title='Error',
